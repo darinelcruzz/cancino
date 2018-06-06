9b5ae7d57f518d9a5a0708e474bc0b251e538c56
Victor <victorjcg_6@hotmail.com>
2018-06-06 18:28:56 -0500

    menu alta

---
+++ resources/lang/es/menus/two.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    'Shoppings' => [
+        'title' => 'Compras',
+        'icon' => 'fa fa-archive',
+        'route' => 'admin.shoppings'
+    ],
+
+    'SalesAdmin' => [
+        'title' => 'Ventas',
+        'icon' => 'fa fa-money',
+        'route' => 'admin.sales'
+    ],
+
+    'ExpensesAdmin' => [
+        'title' => 'Saldos',
+        'icon' => 'fa fa-money',
+        'route' => 'admin.balances'
+    ],
+
+];
--- resources/views/lte/sidebar.blade.php
+++ resources/views/lte/sidebar.blade.php
@@ -1,24 +1,18 @@
 <aside class="main-sidebar">
-
-  <!-- sidebar: style can be found in sidebar.less -->
-  <section class="sidebar">
-
-    <!-- Sidebar Menu -->
-    <ul class="sidebar-menu" data-widget="tree">
-      <li class="header">MENÚ</li>
-
-      @if (auth()->user()->level == 1)
-          @each('lte.items', trans('menus/one'), 'item')
-      @elseif (auth()->user()->level == 4)
-          @each('lte.items', trans('menus/four'), 'item')
-      @elseif (auth()->user()->level == 5)
-          @each('lte.items', trans('menus/five'), 'item')
-      @elseif (auth()->user()->username == 'cheko')
-          @each('lte.items', trans('menus/sergio'), 'item')
-      @endif
-
-    </ul>
-    <!-- /.sidebar-menu -->
-  </section>
-  <!-- /.sidebar -->
+    <section class="sidebar">
+        <ul class="sidebar-menu" data-widget="tree">
+            <li class="header">MENÚ</li>
+            @if (auth()->user()->username == 'cheko')
+                @each('lte.items', trans('menus/sergio'), 'item')
+            @elseif (auth()->user()->level == 1)
+                @each('lte.items', trans('menus/one'), 'item')
+            @elseif (auth()->user()->level == 2)
+                @each('lte.items', trans('menus/two'), 'item')
+            @elseif (auth()->user()->level == 4)
+                @each('lte.items', trans('menus/four'), 'item')
+            @elseif (auth()->user()->level == 5)
+                @each('lte.items', trans('menus/five'), 'item')
+            @endif
+        </ul>
+    </section>
 </aside>
