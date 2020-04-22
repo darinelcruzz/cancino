64fc00767cd8ebe861cf96837cd9087682da8554
Victor Cancino <victorjcg_6@hotmail.com>
2020-04-22 14:19:35 -0500

    commision model refactor

--- app/Commision.php
+++ app/Commision.php
@@ -52,18 +52,28 @@ class Commision extends Model
         return $multiplier[$this->salePoint] * $this->sale;
     }
 
+    function label($point)
+    {
+        $colors = ['rojo' => '#C81D11', 'negro' => '#000000', 'estrella' => '#008F39', 'dorada' => '#DCBF32'];
+        $icons = ['rojo' => 'circle', 'negro' => 'circle', 'estrella' => 'star', 'dorada' => 'star'];
+        $color = array_key_exists($point, $colors) ? $colors[$point] : 'default';
+        $icon = array_key_exists($point, $icons) ? $icons[$point] : 'default';
+
+        return "<i style='color:$color' class='fa fa-$icon'></i>";
+    }
+
     function scPoint($sum_sc)
     {
         $tab = Tab::all()->last();
 
         if($sum_sc > $tab->sc_golden['q']) {
-            return array("$sum_sc </br> <i style='color:#DCBF32' class='fa fa-star'></i> </br>", $tab->sc_golden['a'] * $sum_sc);
+            return array("$sum_sc </br>" . $this->label('dorada') . "<br>", $tab->sc_golden['a'] * $sum_sc);
         }if($sum_sc > $tab->sc_star['q']) {
-            return array("$sum_sc </br> <i style='color:#008F39' class='fa fa-star'></i> </br>", $tab->sc_star['a'] * $sum_sc);
+            return array("$sum_sc </br>" . $this->label('estrella') . "<br>", $tab->sc_star['a'] * $sum_sc);
         }if($sum_sc > $tab->sc_black['q']) {
-            return array("$sum_sc </br> <i style='color:#000000' class='fa fa-circle'></i> </br>", $tab->sc_black['a'] * $sum_sc);
+            return array("$sum_sc </br>" . $this->label('negro') . "<br>", $tab->sc_black['a'] * $sum_sc);
         }else {
-            return array("$sum_sc </br> <i style='color:#C81D11' class='fa fa-circle'></i> </br>", 0);
+            return array("$sum_sc </br>" . $this->label('rojo') . "<br>", 0);
         }
     }
 
@@ -72,26 +82,26 @@ class Commision extends Model
         $tab = Tab::all()->last();
 
         if($sum_ext >= $tab->ext_golden['q']) {
-            return array("$sum_ext </br> <i style='color:#DCBF32' class='fa fa-star'></i> </br>(" . fnumber($sum_amount) . ")</br>", $tab->ext_golden['p'] * $sum_amount);
+            return array("$sum_ext </br>" . $this->label('dorada') . "</br>(" . fnumber($sum_amount) . ")</br>", $tab->ext_golden['p'] * $sum_amount);
         }if($sum_ext >= $tab->ext_star['q']) {
-            return array("$sum_ext </br> <i style='color:#008F39' class='fa fa-star'></i> </br>(" . fnumber($sum_amount) . ")</br>", $tab->ext_star['p'] * $sum_amount);
+            return array("$sum_ext </br>" . $this->label('estrella') . "</br>(" . fnumber($sum_amount) . ")</br>", $tab->ext_star['p'] * $sum_amount);
         }if($sum_ext >= $tab->ext_black['q']) {
-            return array("$sum_ext </br> <i style='color:#000000' class='fa fa-circle'></i> </br>(" . fnumber($sum_amount) . ")</br>", $tab->ext_black['p'] * $sum_amount);
+            return array("$sum_ext </br>" . $this->label('negro') . "</br>(" . fnumber($sum_amount) . ")</br>", $tab->ext_black['p'] * $sum_amount);
         }else {
-            return array("$sum_ext </br> <i style='color:#C81D11' class='fa fa-circle'></i> </br>", 0);
+            return array("$sum_ext </br>" . $this->label('rojo') . "</br>", 0);
         }
     }
 
     function managerPayment($sale, $past_goal, $goal)
     {
         if($sale > ($past_goal->sale * $goal->star * $goal->golden)) {
-            return "<i style='color:#DCBF32' class='fa fa-star'></i> &nbsp; &nbsp; " . fnumber($sale * 0.0075);
+            return  $this->label('dorada') . "&nbsp; &nbsp; " . fnumber($sale * 0.0075);
         }if($sale > ($past_goal->sale * $goal->star)) {
-            return "<i style='color:#008F39' class='fa fa-star'></i> &nbsp; &nbsp; " . fnumber($sale * 0.005);
+            return $this->label('estrella') . "&nbsp; &nbsp; " . fnumber($sale * 0.005);
         }if($sale > ($past_goal->sale * $goal->black)) {
-            return "<i style='color:#000000' class='fa fa-circle'></i> &nbsp; &nbsp; " . fnumber($sale * 0.0025);
+            return $this->label('negro') . "&nbsp; &nbsp; " . fnumber($sale * 0.0025);
         }else {
-            return "<i style='color:#C81D11' class='fa fa-circle'></i> &nbsp; &nbsp; " . fnumber(0);
+            return $this->label('rojo') . "&nbsp; &nbsp; " . fnumber(0);
         }
     }
 
