a7803489f3591d715864b7c40f2650af2fe12341
Victor Cancino <victorjcg_6@hotmail.com>
2020-06-05 13:09:25 -0500

    excel to mac

--- config/excel.php
+++ config/excel.php
@@ -164,7 +164,7 @@ return [
         |
         */
         'local_path'  => sys_get_temp_dir(),
-
+        //'local_path'  => storage_path(),
         /*
         |--------------------------------------------------------------------------
         | Remote Temporary Disk
