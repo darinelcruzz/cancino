07a8ae523492b092638b23c1e9cf313c33357d8b
Darinel Cruz <darinelcruzz@gmail.com>
2020-06-29 14:36:56 -0500

    Error at create payment fixed!

--- app/Http/Controllers/ServicePaymentController.php
+++ app/Http/Controllers/ServicePaymentController.php
@@ -37,7 +37,7 @@ class ServicePaymentController extends Controller
             'expired_at' => $expired_at->add($service->period . " month")
         ]);
 
-        return redirect(route('service.show', $service));
+        return redirect(route('services.show', $service));
     }
 
     function show(ServicePayment $servicePayment)
