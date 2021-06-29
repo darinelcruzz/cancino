6e48e16bab65b25412b717325c985ae8bb5b9d99
VictorCacino <victorjcg_6@hotmail.com>
2021-06-28 20:17:19 -0500

    inventary menu edition

--- resources/lang/es/menus/eight.php
+++ resources/lang/es/menus/eight.php
@@ -2,74 +2,22 @@
 
 return [
 
-    'Inventory' => [
-        'title' => 'Inventarios',
-        'icon' => 'fa fa-people-carry',
-        'submenu' => [
-            'new' => [
-                'title' => 'Historial',
-                'route' => 'inventory.index'
-            ],
-            'index' => [
-                'title' => 'Conteos',
-                'route' => 'count.index'
-            ],
-            'products' => [
-                'title' => 'Productos',
-                'route' => 'product.index'
-            ],
-            'locations' => [
-                'title' => 'Ubicaciones',
-                'route' => 'location.create'
-            ],
-            'upload' => [
-                'title' => 'Excel',
-                'route' => 'inventory.upload'
-            ],
-        ],
-    ],
-
     'counts' => [
-        'title' => 'Conteos',
+        'title' => 'Agregar normal',
         'icon' => 'fa fa-calculator',
-        'submenu' => [
-            'partials' => [
-                'title' => 'Parciales',
-                'route' => 'inventory.partial.index'
-            ],
-            'create' => [
-                'title' => 'Agregar normal',
-                'route' => ['count.create', 'normal']
-            ],
-            'create2' => [
-                'title' => 'Agregar con código de barras',
-                'route' => ['count.create', 'codigo-de-barras']
-            ],
-        ]
+        'route' => ['count.create', 'normal']
+    ],
+    'barcode' => [
+        'title' => 'Agregar con código de barras',
+        'icon' => 'fa fa-barcode',
+        'route' => ['count.create', 'codigo-de-barras']
+    ],
+    'Inventory' => [
+        'title' => 'Historial',
+        'icon' => 'fa fa-people-carry',
+        'route' => 'count.index'
     ],
 
-    'supplies' => [
-        'title' => 'Insumos',
-        'icon' => 'fa fa-broom',
-        'submenu' => [
-            'sales' => [
-                'title' => 'Ventas',
-                'route' => 'supplies.sales.index'
-            ],
-            'purchases' => [
-                'title' => 'Compras',
-                'route' => 'supplies.purchases.index'
-            ],
-            'products' => [
-                'title' => 'Productos',
-                'route' => 'supplies.index'
-            ],
-            'upload' => [
-                'title' => 'Excel',
-                'route' => 'inventory.upload'
-            ],
-        ],
-    ],
 
     'logout' => [
         'title' => 'Salir',
